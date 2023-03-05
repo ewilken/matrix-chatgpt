f74c7277da5f1b4b131ac94362fe3d3e761c003f
Elias Wilken <elias@wlkn.io>
2023-03-05 23:21:32 +0100

    wire it up to ChatGPT

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Matrix bridge to the ChatGPT API.
 
     docker build -t ghcr.io/ewilken/matrix-chatgpt .
     docker run \
+      -e OPENAI_API_KEY="yourkey" \
       -e MATRIX_USERNAME="@chatgpt:yourhomeserver.tld" \
       -e MATRIX_PASSWORD="yourpassword" \
       ghcr.io/ewilken/matrix-chatgpt
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
 use anyhow::Result;
+use async_openai::{
+    types::{ChatCompletionRequestMessage, CreateChatCompletionRequest, Role},
+    Chat, Client as OpenAIClient,
+};
+use lazy_static::lazy_static;
 use matrix_sdk::{
     config::SyncSettings,
     room::Room,
     ruma::{
         events::room::{
             member::StrippedRoomMemberEvent,
-            message::{
-                MessageType, OriginalSyncRoomMessageEvent, RoomMessageEventContent,
-                SyncRoomMessageEvent,
-            },
+            message::{MessageType, RoomMessageEventContent, SyncRoomMessageEvent},
         },
         UserId,
     },
-    Client,
+    Client as MatrixClient,
 };
 use std::{env, time::Duration};
 use tracing::{debug, error, info};
+
+lazy_static! {
+    static ref OPENAI_CLIENT: OpenAIClient = {
+        let openai_api_key = env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY must be set");
+        let openai_client = OpenAIClient::new().with_api_key(openai_api_key);
+        openai_client
+    };
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -25,38 +35,41 @@ async fn main() -> Result<()> {
     let matrix_password = env::var("MATRIX_PASSWORD").expect("MATRIX_PASSWORD must be set");
     let matrix_user_id = UserId::parse(matrix_username)?;
 
-    let client = Client::builder()
+    let matrix_client = MatrixClient::builder()
         .server_name(matrix_user_id.server_name())
         .respect_login_well_known(true)
         .handle_refresh_tokens()
         .build()
         .await?;
 
-    client
+    matrix_client
         .login_username(&matrix_user_id, &matrix_password)
         .initial_device_display_name("ChatGPT")
         .send()
         .await?;
 
-    client.add_event_handler(on_stripped_state_member);
+    matrix_client.add_event_handler(on_stripped_state_member);
 
     // An initial sync to set up state and so our bot doesn't respond to old messages.
-    let sync_token = client.sync_once(SyncSettings::default()).await?.next_batch;
+    let sync_token = matrix_client
+        .sync_once(SyncSettings::default())
+        .await?
+        .next_batch;
 
-    client.add_event_handler(on_room_message);
+    matrix_client.add_event_handler(on_room_message);
 
     // Since we called `sync_once` before we entered our sync loop, we must pass that sync token to `sync`.
     let settings = SyncSettings::default().token(sync_token);
 
     // Syncing is important to synchronize the client state with the server.
     // This method will never return.
-    client.sync(settings).await?;
+    matrix_client.sync(settings).await?;
 
     Ok(())
 }
 
-async fn on_room_message(event: SyncRoomMessageEvent, room: Room, client: Client) {
-    info!("Received event {:?} in room {:?}", event, room);
+async fn on_room_message(event: SyncRoomMessageEvent, room: Room, client: MatrixClient) {
+    debug!("Received event {:?} in room {:?}", event, room);
 
     if event.sender() == client.user_id().unwrap() {
         // Skip messages sent by the bot.
@@ -68,15 +81,45 @@ async fn on_room_message(event: SyncRoomMessageEvent, room: Room, client: Client
     let Room::Joined(room) = room else { return };
     let MessageType::Text(ref text_content) = event.content.msgtype else { return };
 
-    info!("Received message {}", text_content.body);
+    debug!("Received message: {}", text_content.body);
 
-    let response_content = RoomMessageEventContent::text_plain("test response");
-    room.send(response_content, None).await;
+    let chatgpt_request = CreateChatCompletionRequest {
+        model: "gpt-3.5-turbo".into(),
+        messages: vec![ChatCompletionRequestMessage {
+            role: Role::User,
+            content: text_content.body.to_string(),
+            name: None, // TODO: get user name
+        }],
+        temperature: None,
+        top_p: None,
+        n: Some(1),
+        stream: Some(false),
+        stop: None,
+        max_tokens: None,
+        presence_penalty: None,
+        frequency_penalty: None,
+        logit_bias: None,
+        user: Some("matrix-chatgpt".into()),
+    };
+    let Ok(chatgpt_response) = Chat::new(&OPENAI_CLIENT).create(chatgpt_request).await else { return; };
+
+    let response = chatgpt_response.choices[0].message.content.clone();
+
+    debug!("Sending ChatGPT response: {}", response);
+
+    let response_content = RoomMessageEventContent::text_markdown(response);
+
+    room.send(response_content, None)
+        .await
+        .map_err(|e| {
+            error!("Failed to send message: {:?}", e);
+        })
+        .ok();
 }
 
 async fn on_stripped_state_member(
     room_member: StrippedRoomMemberEvent,
-    client: Client,
+    client: MatrixClient,
     room: Room,
 ) {
     if room_member.state_key != client.user_id().unwrap() {
